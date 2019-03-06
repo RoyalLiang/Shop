a1f9d7eeff5644dc845cb5a77c1c2992a03f38d4
RoyalLiang <royal.liang.459@gmail.com>
2019-03-06 17:50:22 +0800

    使用serve配置上下文管理器

--- Shop/settings.py
+++ Shop/settings.py
@@ -74,6 +74,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 上下文管理器
+                'django.template.context_processors.media',
             ],
         },
     },
