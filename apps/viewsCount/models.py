25d0994b8bfba02c632c5dbba7806a8d69757388
WPN0837 <37870922+WPN0837@users.noreply.github.com>
2019-03-18 00:53:01 +0800

    增加了采集用户浏览行为数据的功能 js从前端采集数据，在页面加载完成后发送到后台，异步到数据库

--- apps/viewsCount/models.py
+++ apps/viewsCount/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.db import models
 
 
@@ -6,12 +5,15 @@ class Visitor(models.Model):
     '''
     访客信息
     '''
-    ip = models.GenericIPAddressField(verbose_name='IP')
+    in_ip = models.GenericIPAddressField(verbose_name='内网')
+    pub_ip = models.GenericIPAddressField(verbose_name='公网')
+    url = models.URLField(verbose_name='url', null=True, blank=True)
     refer = models.URLField(verbose_name='来源', null=True, blank=True)
+    timeIn = models.CharField(max_length=16, verbose_name='进入时间戳', null=True, )
+    time = models.IntegerField(verbose_name='停留时间', null=True, )
+    timeOut = models.CharField(max_length=16, verbose_name='离开时间戳', null=True, )
     user_agent = models.CharField(max_length=500, verbose_name='浏览器信息', null=True, blank=True)
     address = models.CharField(max_length=500, verbose_name='ip地区', null=True, blank=True)
-    count = models.IntegerField(default=1, verbose_name='访问次数', blank=True)
-    time = models.DateTimeField(default=datetime.now, verbose_name='时间', blank=True)
 
     class Meta:
         verbose_name = '访客信息'
@@ -19,4 +21,4 @@ class Visitor(models.Model):
         unique_together = []
 
     def __str__(self):
-        return self.ip + "--" + str(self.count)
+        return '%s/%s' % (self.pub_ip, self.in_ip)
--- apps/viewsCount/tasks.py
+++ apps/viewsCount/tasks.py
@@ -1,13 +1,44 @@
 from Shop.celery import app
 from .models import Visitor
+import json
 
 
 @app.task
 def views_count_save(request):
     if request.META.get('HTTP_X_FORWARDED_FOR', None):
-        ip = request.META['HTTP_X_FORWARDED_FOR']
+        in_ip = request.META['HTTP_X_FORWARDED_FOR']
     else:
-        ip = request.META['REMOTE_ADDR']
-    refer = request.META.get('HTTP_REFERER')
-    user_agent = request.META.get('HTTP_USER_AGENT')
-    Visitor(ip=ip, refer=refer, user_agent=user_agent).save()
+        in_ip = request.META['REMOTE_ADDR']
+    data_json = request.POST.get('data', None)
+    if data_json:
+        obj = {}
+        data_dic = json.loads(data_json)
+        obj['in_ip'] = in_ip
+        obj['pub_ip'] = data_dic.get('ip', None)
+        obj['address'] = data_dic.get('address', None)
+        user_agent = data_dic.get('user_agent', None)
+        if user_agent:
+            user_agent_dic = json.loads(user_agent)
+            user_agent = "%s:%s" % (user_agent_dic['terminal'], user_agent_dic['browser'])
+        obj['user_agent'] = user_agent
+        tjArr = data_dic.get('tjArr', None)
+        url = None
+        refer = None
+        timeIn = None
+        time = None
+        timeOut = None
+        if tjArr:
+            tjArr_dic = json.loads(tjArr)
+            url = tjArr_dic.get('url', None)
+            refer = tjArr_dic.get('refer', None)
+            timeIn = tjArr_dic.get('timeIn', None)
+            time = tjArr_dic.get('time', None)
+            timeOut = tjArr_dic.get('timeOut', None)
+        obj['url'] = url
+        obj['refer'] = refer
+        obj['timeIn'] = str(timeIn)
+        obj['time'] = time
+        obj['timeOut'] = str(timeOut)
+        Visitor(**obj).save()
+        return True
+    return False
