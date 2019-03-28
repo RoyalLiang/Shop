d50b01b149b979be0a2a44eda050191cacce6958
RoyalLiang <royal.liang.459@gmail.com>
2019-03-28 13:51:56 +0800

    新页面

---
+++ apps/viewsCount/urls.py
@@ -0,0 +1,8 @@
+# from django.urls import path
+# from .views import TestView
+#
+# app_name = 'ViewsCount'
+# urlpatterns = [
+#     # path('brower-info/', GetData.as_view(), name='brower-info'),
+#     # path('test/', TestView.as_view(), name='testview')
+# ]
