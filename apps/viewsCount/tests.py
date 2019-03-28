27a2e728ca038a82a4f6f75d868f1c1eaaa674e7
WPN0837 <37870922+WPN0837@users.noreply.github.com>
2019-03-28 14:11:01 +0800

    图片加alt

--- apps/viewsCount/tests.py
+++ apps/viewsCount/tests.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import time
 
-t = datetime.now
-print(t)
+start = time.mktime(time.strptime("%s 00:00:00" % time.strftime("%Y-%m-%d", time.localtime(time.time() - 24 * 60 * 60)),
+                                  "%Y-%m-%d %H:%M:%S"))
+end = int(start) + 24 * 60 * 60 - 1
